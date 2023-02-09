cb2fccb2e8b5cdc44032b81157c71ea87601e6a1
ADM <dimahudobin05@gmail.com>
2023-02-09 21:37:52 +0700

    home1

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -5,7 +5,6 @@ export default new Vuex.Store({
   state: {
     status: '',
     token: localStorage.getItem('token') || '',
-    user: {},
     API: 'https://jurapro.bhuser.ru/api-shop/'
   },
   getters: {
@@ -15,30 +14,28 @@ export default new Vuex.Store({
     auth_request(state){
       state.status = 'loading'
     },
-    auth_success(state, token, user){
+    auth_success(state, token){
       state.status = 'success'
       state.token = token
-      state.user = user
     },
     auth_error(state){
       state.status = 'error'
     },
-    logout(state){
-      state.status = ''
-      state.token = ''
+    logout(state, token){
+      state.status = 'logout'
+      state.token = token
     }
   },
   actions: {
     login({commit}, user){
       return new Promise((resolve, reject) => {
         commit('auth_request')
-        axios.post( this.state.API + 'login', user )
+        axios.post( this.state.API + 'login')
             .then( resp => {
               const token = resp.data.data.user_token
-              const user = resp.data.data.user
               localStorage.setItem('token', token)
               axios.defaults.headers.common['Authorization'] = token
-              commit('auth_success', token, user)
+              commit('auth_success', token)
               resolve(resp)
             })
             .catch(err => {
@@ -51,13 +48,12 @@ export default new Vuex.Store({
     signup({commit}, user) {
       return new Promise((resolve, reject) => {
         commit('auth_request')
-        axios.post(this.state.API + 'signup', user)
+        axios.post(this.state.API + 'signup')
             .then(resp => {
               const token = resp.data.data.user_token
-              const user = resp.data.user
               localStorage.setItem('token', token)
               axios.defaults.headers.common['Authorization'] = token
-              commit('auth_success', token, user)
+              commit('auth_success', token)
               resolve(resp)
             })
             .catch(err => {
@@ -67,7 +63,20 @@ export default new Vuex.Store({
             })
       })
     },
-
+      logout({commit}){
+        return new Promise((resolve) => {
+        commit('logout')
+          axios.get(this.state.API + 'logout')
+              .then(resp => {
+                  const token = ''
+                  localStorage.removeItem('token')
+                  axios.defaults.headers.common['Authorization'] = ''
+                  commit('logout', token)
+                  resolve(resp)
+              })
+          // localStorage.removeItem('token')
+        })
+      },
   },
   modules: {
   },
